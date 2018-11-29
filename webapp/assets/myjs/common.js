56366c1988926207cd16bb7f3cca3d3ab4b9c547
Eiden J.P Zhou <303713060@qq.com>
2018-11-29 17:49:31 +0800

    123

--- webapp/assets/myjs/common.js
+++ webapp/assets/myjs/common.js
@@ -8,3 +8,4 @@ document.write('<link rel="stylesheet" href="../assets/css/ace-ie.css" />');
 document.write('<script src="../assets/js/html5shiv.js"></script>');
 document.write('<script src="../assets/js/respond.js"></script>');
 document.write('<script src="../js/jquery.min.js"></script>');
+document.write('<script src="../js/jquery.slider.min.js"></script>');
